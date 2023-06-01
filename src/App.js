e42f72e063f4c9abf30a263c13f24b54b434aa3e
Elyse Newland <elysenewland@gmail.com>
2023-06-01 13:11:13 -0400

    Update fetch URLs from local host to deployed server

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ class App extends Component {
       return;
     }
     this.setState({ imageUrl: this.state.input });
-    fetch("http://localhost:3000/imageurl", {
+    fetch("https://facefinder-backend.onrender.com/imageurl", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -96,7 +96,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("http://localhost:3000/image", {
+          fetch("https://facefinder-backend.onrender.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
